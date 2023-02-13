da7580e0331c2f1c9ea3583be545ca640ed0a58c
ujjawal0203 <ujjawal0203.jk@gmail.com>
2023-02-13 21:44:02 +0530

    upDate

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -10,20 +10,29 @@ const Portfolio = () => {
         {
             id:1,
             src: Lately,
+            title:"Lately.com",
             glink:"https://github.com/Ujjawal0203/racial-spiders-3687",
-            nlink:"https://harmonious-semolina-d8a151.netlify.app/"
+            nlink:"https://harmonious-semolina-d8a151.netlify.app/",
+            detail:"This is a clone of Coschedule website, it helps us organise our days accordingly and save out time and help us in time management.",
+            tech:"Tech Stack: HTML | CSS | JavaScript"
         },
         {
             id:2,
             src: Jefit,
+            title:"Jefit.com",
             glink:"https://github.com/Ujjawal0203/aquatic-discussion-8005",
-            nlink:"https://venerable-boba-d7e055.netlify.app/"
+            nlink:"https://venerable-boba-d7e055.netlify.app/",
+            detail:"This is a clone of Coschedule website, it helps us organise our days accordingly and save out time and help us in time management.",
+            tech:"Tech Stack: HTML | CSS | JavaScript"
         },
         {
             id:3,
             src: Fiverr,
+            title:"Fiverr.com",
             glink:"https://github.com/Ujjawal0203/minor-crowd-1344/tree/main/my-project",
-            nlink:"https://inspiring-raindrop-9d2b46.netlify.app/"
+            nlink:"https://inspiring-raindrop-9d2b46.netlify.app/",
+            detail:"This is a clone of Coschedule website, it helps us organise our days accordingly and save out time and help us in time management.",
+            tech:"Tech Stack: HTML | CSS | JavaScript | React"
         },
       
     ]
@@ -36,10 +45,13 @@ const Portfolio = () => {
                 <p className='py-6'>Check out some of my work right here</p>
             </div>
 
-          <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-            {portfolios.map(({id,src,nlink,glink})=>(
+          <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0' style={{textAlign:"center"}} >
+            {portfolios.map(({id,src,nlink,glink,detail,title,tech})=>(
             <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
             <img src={src} alt="" className='rounded-md duration-500 hover:scale-105' />
+            <div className='mt-2 text-xl'>{title}</div>
+            <div className='mt-2'>{detail}</div>
+            <div className='mt-2 text-gray-400'>{tech}</div>
             <div className='flex justify-center items-center'>
             <a href={nlink} target="_blank" rel="noreferrer">
                 <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110'>Demo</button>
